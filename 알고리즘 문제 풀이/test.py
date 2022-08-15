3bc82ef81a69dd98f5c126822d203091c9956304
gusrn0505 <gusrn0505@naver.com>
2022-08-16 00:00:22 +0900

    22.08.15 알고리즘 풀이

--- 알고리즘 문제 풀이/test.py
+++ 알고리즘 문제 풀이/test.py
@@ -1,3 +1,27 @@
-import numpy as np
-lst = [1,2,3]
-print(np.array([0]*5))
+import math
+def draw_star(n) :
+    if n ==1 : return [["*"]]
+    a = draw_star(n-1)
+    b = a + [[" "]*len(a)]*len(a) + a
+    a = a*3
+    c = [a[i] + b[i] + a[i] for i in range(len(a))]
+
+    return c
+
+num = math.log(int(input()), 3) +1
+
+for _ in draw_star(num) :
+    print("".join(_))
+
+"""
+b = a + [[" "]*len(a)]*len(a) + a
+a = a*3
+c = [a[i] + b[i] + a[i] for i in range(len(a))]
+
+print(a)
+print(b)
+# 이중 리스트에 곱하기를 하면 밑으로 복사가 되고,
+
+for _ in c :
+    print("".join(_))
+"""
